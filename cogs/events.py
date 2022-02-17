7744fd973c40274d5c0c1249ad02cf398d5b22e9
Anshuman006 <anshuman0006@yahoo.com>
2022-02-17 20:13:03 +0530

    Temp Fix

--- cogs/events.py
+++ cogs/events.py
@@ -28,8 +28,8 @@ class Events(commands.Cog):
             if message.author.id == self.bot.user.id:
                 return
 
-            if await B_Data.get_or_none(user_id=message.author.id):
-                return
+            # if await B_Data.get_or_none(user_id=message.author.id):
+            #     return
             
             record = await self.bot.db.fetchrow('SELECT * FROM afk WHERE (guild_id,user_id) = ($1,$2)', 0, message.author.id)
             if record and message.content.startswith('#'):
